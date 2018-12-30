a9a97e1e50546dc602448da13b6e4408f6837bdc
Orlin Nedyalkov <orlinbox@gmail.com>
2018-12-30 14:58:38 +0200

    Simple slider.

--- simple-slider.js
+++ simple-slider.js
@@ -37,16 +37,33 @@
     ss.clone().removeClass('js-ss').addClass('js-ss-placeholder').attr('aria-hidden', true).insertAfter(ss);
     // init
     simsl(arr);
-    // click / keyboard focus
+    // click / keyboard (enter key)
     $('.js-ss-next', this).click(function() { simslNext(arr); }).on('keydown', function(e) { if (e.which == 13) { simslNext(arr); } });
     $('.js-ss-prev', this).click(function() { simslPrev(arr); }).on('keydown', function(e) { if (e.which == 13) { simslPrev(arr); } });
-    // keyboard arrows
+    // keyboard (left and right arrow keys)
     $('body').on('keydown', function(e) {
       if (e.which == 39) { simslNext(arr); }
       if (e.which == 37) { simslPrev(arr); }
     });
-    // swipe (require pure-swipe addon)
-    ss[0].addEventListener('swiped-left', function() { simslNext(arr); });
-    ss[0].addEventListener('swiped-right', function() { simslPrev(arr); });
+    // swipe (on touch devices)
+    ss[0].addEventListener('touchstart', handleTouchStart, false);
+    ss[0].addEventListener('touchmove', handleTouchMove, false);
+    ss[0].addEventListener('touchend', handleTouchEnd, false);
+    var xDown, xDiff, timeDown, startEl;
+    function handleTouchStart(e) {
+      startEl = e.target;
+      timeDown = Date.now();
+      xDown = e.touches[0].clientX;
+      xDiff = 0;
+    }
+    function handleTouchMove(e) { xDiff = xDown - e.touches[0].clientX; }
+    function handleTouchEnd(e) {
+      if (startEl !== e.target) return;
+      var swipeThreshold = 30; // px
+      var swipeTimeout = 500; // ms
+      if (Math.abs(xDiff) > swipeThreshold && (Date.now() - timeDown) < swipeTimeout) {
+        if (xDiff > 0) { simslNext(arr); } else { simslPrev(arr); }
+      }
+    }
   });
 })();
