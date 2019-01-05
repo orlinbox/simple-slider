214853bad3045cb7bd1a06733169761df40cac25
Orlin Nedyalkov <orlinbox@gmail.com>
2019-01-05 21:26:47 +0200

    Simple slider.

--- simple-slider.js
+++ simple-slider.js
@@ -1,5 +1,5 @@
 /*
-  <div class="js-simple-slider" role="region" aria-label="Simple slider">
+  <div class="js-simple-slider" data-ss-delay="7500" role="region" aria-label="Simple slider">
     <ul class="js-ss">
       <li>Slide content</li>
     </ul>
@@ -9,28 +9,28 @@
   </div>
 */
 (function() {
-  function simsl(goTo, sli) {
-    var el = $(sli);
-    var pos = parseInt(el.attr('data-ss-position'));
+  function simsl(goTo, sli, disable) {
+    if (disable) clearInterval(sli.int);
+    var pos = parseInt(sli.attr('data-ss-position') - 1);
     if (goTo !== pos) {
-      var len = parseInt(el.attr('data-ss-length'));
+      var len = parseInt(sli.attr('data-ss-length'));
       if (goTo === 9999) goTo = pos + 1;
       if (goTo === -9999) goTo = pos - 1;
       if (goTo > (len-1)) goTo = 0;
       if (goTo < 0) goTo = len - 1;
-      el.attr('data-ss-position', goTo);
+      sli.attr('data-ss-position', goTo + 1);
       goToNext = goTo + 1;
       if (goToNext > (len-1)) goToNext = 0;
       if (goToNext < 0) goToNext = len - 1;
       goToPrev = goTo - 1;
       if (goToPrev > (len-1)) goToPrev = 0;
       if (goToPrev < 0) goToPrev = len - 1;
-      $('.js-ss li', el).each(function(index) {
+      $('.js-ss li', sli).each(function(index) {
         var elm = $(this);
         elm.removeAttr('class').attr('aria-hidden', true);
         if (index === goTo) {
           elm.addClass('js-ss-sl-current').removeAttr('aria-hidden');
-          $('.js-ss-nav li', el).removeAttr('class').eq(goTo).addClass('active');
+          $('.js-ss-nav li', sli).removeAttr('class').eq(goTo).addClass('active');
         } else if (index === goToNext) { elm.addClass('js-ss-sl-right');
         } else if (index === goToPrev) { elm.addClass('js-ss-sl-left');
         } else { elm.addClass('js-ss-sl'); }
@@ -41,7 +41,7 @@
   $('.js-simple-slider').each(function() {
     var ss = $('.js-ss', this);
     var elm = $('li', ss);
-    var sli = $(this).attr('data-ss-position', -1).attr('data-ss-length', elm.length);
+    var sli = $(this).attr('data-ss-position', 0).attr('data-ss-length', elm.length);
     var dots = $('.js-ss-nav', this);
     elm.each(function() { dots.append('<li></li>'); });
     // clone (for placeholder purposes)
@@ -49,15 +49,23 @@
     ss.clone().removeClass('js-ss').addClass('js-ss-placeholder').attr('aria-hidden', true).insertAfter(ss);
     // init
     simsl(0, sli);
+    // autorotate
+    var timer = sli.attr('data-ss-delay') || 0;
+    if (timer > 0) sli.int = setInterval(function(){ simsl(9999, sli); }, timer);
+    // click prev/next image
+    elm.click(function() {
+      if ($(this).hasClass('js-ss-sl-left')) simsl(-9999, sli, true);
+      if ($(this).hasClass('js-ss-sl-right')) simsl(9999, sli, true);
+    });
     // click / keyboard (enter key)
-    $('.js-ss-next', this).click(function() { simsl(9999, sli); }).on('keydown', function(e) { if (e.which == 13) { simsl(9999, sli); } });
-    $('.js-ss-prev', this).click(function() { simsl(-9999, sli); }).on('keydown', function(e) { if (e.which == 13) { simsl(-9999, sli); } });
+    $('.js-ss-next', this).click(function() { simsl(9999, sli, true); }).on('keydown', function(e) { if (e.which == 13) { simsl(9999, sli, true); } });
+    $('.js-ss-prev', this).click(function() { simsl(-9999, sli, true); }).on('keydown', function(e) { if (e.which == 13) { simsl(-9999, sli, true); } });
     // click on dot
-    $('li', dots).each(function(index) { $(this).click(function() { simsl(index, sli); }); });
+    $('li', dots).each(function(index) { $(this).click(function() { simsl(index, sli, true); }); });
     // keyboard (left and right arrow keys)
     $('body').on('keydown', function(e) {
-      if (e.which == 39) { simsl(9999, sli); }
-      if (e.which == 37) { simsl(-9999, sli); }
+      if (e.which == 39) { simsl(9999, sli, true); }
+      if (e.which == 37) { simsl(-9999, sli, true); }
     });
     // swipe (on touch devices)
     ss[0].addEventListener('touchstart', handleTouchStart, false);
@@ -76,7 +84,7 @@
       var swipeThreshold = 25; // px
       var swipeTimeout = 500; // ms
       if (Math.abs(xDiff) > swipeThreshold && (Date.now() - timeDown) < swipeTimeout) {
-        if (xDiff > 0) { simsl(9999, sli); } else { simsl(-9999, sli); }
+        if (xDiff > 0) { simsl(9999, sli, true); } else { simsl(-9999, sli, true); }
       }
     }
   });
