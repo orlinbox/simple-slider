c9f739214aa540258a9937a08c2745d730956492
Orlin Nedyalkov <orlin.nedyalkov@sitecore.com>
2019-11-28 15:33:04 +0200

    Update to ES6.

--- gulpfile.js
+++ gulpfile.js
@@ -1,52 +1,62 @@
 /* Includes ----------------------------------------------------------------- */
 
-var fs = require('fs');
-var respath = require('path');
-var gulp = require('gulp');
-var color = require('colors');
-var plumber = require('gulp-plumber');
-var sass = require('gulp-sass');
-var jshint = require('gulp-jshint');
-var notify = require("gulp-notify");
-var gulp_css_count = require('gulp-css-count');
+const fs = require('fs');
+const respath = require('path');
+const gulp = require('gulp');
+const color = require('colors');
+const plumber = require('gulp-plumber');
+const sass = require('gulp-sass');
+const babel = require('gulp-babel');
+const notify = require("gulp-notify");
+const gulpCssCount = require('gulp-css-count');
+
+/* Helpers ------------------------------------------------------------------ */
+
+const logInfo = file => {
+  const path = file;
+  const absolutePath = respath.resolve(path);
+  const stats = fs.statSync(path);
+  console.log('\n' + color.cyan(absolutePath) + ' ' +  color.yellow((stats.size/1000).toFixed(2) +' kB'));
+}
 
 /* Styles ------------------------------------------------------------------- */
 
-function styles() {
-  var onError = function(err) { notify.onError({title: "SASS"})(err); this.emit('end'); };
+const styles = () => {
+  const onError = (err) => {
+    notify.onError({ title: 'SASS' })(err);
+    this.emit('end');
+  };
   return gulp.src('simple-slider.scss')
     .pipe(plumber({errorHandler: onError}))
     .pipe(sass({outputStyle: 'expanded'})) /* compressed / expanded */
     .pipe(gulp.dest('./'));
 }
 
-function stylesCount() { return gulp.src('simple-slider.css').pipe(gulp_css_count()); }
+const stylesCount = () => gulp.src('simple-slider.css').pipe(gulpCssCount());
 
 /* Scripts ------------------------------------------------------------------ */
 
-function scriptsLint() {
-  var onError = function(err) { notify.onError({title: "JS"})(err); this.emit('end'); };
-  return gulp.src('simple-slider.js')
+const scripts = () => {
+  const onError = (err) => {
+    notify.onError({ title: 'JS' })(err);
+    this.emit('end');
+  };
+  const file = 'simple-slider.js';
+  return gulp.src(`./src/${file}`)
     .pipe(plumber({errorHandler: onError}))
-    .pipe(jshint())
-    .pipe(jshint.reporter('default'))
-    .pipe(jshint.reporter('fail'))
-    .on('end', function() { logInfo('simple-slider.js'); });
+    .pipe(babel({
+      presets: ['@babel/env'],
+    }))
+    .pipe(gulp.dest('./'))
+    .on('end', () => logInfo(file));
 }
 
-/* Helpers ------------------------------------------------------------------ */
+/* Gulp --------------------------------------------------------------------- */
 
-function logInfo(file) {
-  var path = file;
-  var absolutePath = respath.resolve(path);
-  var stats = fs.statSync(path);
-  console.log('\n' + color.cyan(absolutePath) + ' ' +  color.yellow((stats.size/1000).toFixed(2) +' kB'));
-}
+const buildStyles = gulp.series(styles, stylesCount);
+const build = gulp.parallel(buildStyles, scripts);
 
-var buildStyles = gulp.series(styles, stylesCount);
-var build = gulp.parallel(buildStyles, scriptsLint);
-
-function watchNow() { gulp.watch(['simple-slider.js', 'simple-slider.scss'], build); }
+const watchNow = () => gulp.watch(['src/simple-slider.js', 'simple-slider.scss'], build);
 
 gulp.task('watch', gulp.parallel(build, watchNow));
 gulp.task('default', build);
--- src/simple-slider.js
+++ src/simple-slider.js
@@ -1,5 +1,3 @@
-"use strict";
-
 /*!
 
 MIT License | Copyright (c) 2019 | orlinbox | https://github.com/orlinbox/simple-slider
@@ -25,37 +23,38 @@ SS version 2.0
     </div>
   </div>
 */
-(function () {
+
+(() => {
+
   // elements with focus
-  var foc = 'a[href], area[href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), button:not([disabled]), iframe'; // main function
-
-  var simsl = function simsl(goToP, sli, disable) {
-    var goTo = goToP; // disable autorotate feature
-
-    if (disable) clearInterval(sli["int"]); // current position
-
-    var pos = parseInt(sli.attr('data-ss-position') - 1, 10); // move logic and action
-
+  const foc = 'a[href], area[href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), button:not([disabled]), iframe';
+  // main function
+  const simsl = (goToP, sli, disable) => {
+    let goTo = goToP;
+    // disable autorotate feature
+    if (disable) clearInterval(sli.int);
+    // current position
+    const pos = parseInt(sli.attr('data-ss-position') - 1, 10);
+    // move logic and action
     if (goTo !== pos) {
-      var len = parseInt(sli.attr('data-ss-length'), 10);
-      var goToNext = null;
-      var goToPrev = null;
+      const len = parseInt(sli.attr('data-ss-length'), 10);
+      let goToNext = null;
+      let goToPrev = null;
       if (goTo === 9999) goTo = pos + 1;
       if (goTo === -9999) goTo = pos - 1;
-      if (goTo > len - 1) goTo = 0;
+      if (goTo > (len-1)) goTo = 0;
       if (goTo < 0) goTo = len - 1;
       sli.attr('data-ss-position', goTo + 1);
       goToNext = goTo + 1;
-      if (goToNext > len - 1) goToNext = 0;
+      if (goToNext > (len-1)) goToNext = 0;
       if (goToNext < 0) goToNext = len - 1;
       goToPrev = goTo - 1;
-      if (goToPrev > len - 1) goToPrev = 0;
+      if (goToPrev > (len-1)) goToPrev = 0;
       if (goToPrev < 0) goToPrev = len - 1;
-      $('.js-ss li', sli).each(function (index, el) {
-        var li = $(el);
+      $('.js-ss li', sli).each((index, el) => {
+        const li = $(el);
         li.removeAttr('class').attr('aria-hidden', true);
         $(foc, li).attr('tabindex', -1);
-
         if (index === goTo) {
           li.addClass('js-ss-sl-current').removeAttr('aria-hidden');
           $(foc, li).removeAttr('tabindex');
@@ -69,95 +68,78 @@ SS version 2.0
         }
       });
     }
-  }; // each slider
-
-
-  $('.js-simple-slider').each(function (i, el) {
-    var ss = $('.js-ss', el);
-    var li = $('li', ss);
-    var sli = $(el).removeClass('js-ss-nojs').attr('data-ss-position', 0).attr('data-ss-length', li.length);
-    var dots = $('.js-ss-nav', el);
-    li.each(function () {
-      return dots.append('<li></li>');
-    }); // clone slider content (for placeholder purposes) and disable focus
-
+  }
+  // each slider
+  $('.js-simple-slider').each((i, el) => {
+    const ss = $('.js-ss', el);
+    const li = $('li', ss);
+    const sli = $(el).removeClass('js-ss-nojs').attr('data-ss-position', 0).attr('data-ss-length', li.length);
+    const dots = $('.js-ss-nav', el);
+    li.each(() => dots.append('<li></li>'));
+    // clone slider content (for placeholder purposes) and disable focus
     ss.wrap('<div class="js-ss-wrap"></div>');
-    $(foc, ss.clone().removeClass('js-ss').addClass('js-ss-placeholder').attr('aria-hidden', true).insertAfter(ss)).attr('tabindex', -1); // init slider
-
-    simsl(0, sli); // autorotate feature
-
-    var timer = sli.attr('data-ss-delay') || 0;
-    if (timer > 0) sli["int"] = setInterval(function () {
-      return simsl(9999, sli);
-    }, timer); // click prev-next image
-
-    li.click(function (elm) {
+    $(foc, ss.clone().removeClass('js-ss').addClass('js-ss-placeholder').attr('aria-hidden', true).insertAfter(ss)).attr('tabindex', -1);
+    // init slider
+    simsl(0, sli);
+    // autorotate feature
+    const timer = sli.attr('data-ss-delay') || 0;
+    if (timer > 0) sli.int = setInterval(() => simsl(9999, sli), timer);
+    // click prev-next image
+    li.click(elm => {
       if ($(elm.currentTarget).hasClass('js-ss-sl-left')) simsl(-9999, sli, true);
       if ($(elm.currentTarget).hasClass('js-ss-sl-right')) simsl(9999, sli, true);
-    }); // click prev-next button / press keyboard enter key
-
-    $('.js-ss-next', el).click(function () {
-      return simsl(9999, sli, true);
-    }).on('keydown', function (e) {
+    });
+    // click prev-next button / press keyboard enter key
+    $('.js-ss-next', el).click(() => simsl(9999, sli, true))
+    .on('keydown', e => {
       if (e.originalEvent.key === 'Enter') simsl(9999, sli, true);
     });
-    $('.js-ss-prev', el).click(function () {
-      return simsl(-9999, sli, true);
-    }).on('keydown', function (e) {
+    $('.js-ss-prev', el).click(() => simsl(-9999, sli, true))
+    .on('keydown', e => {
       if (e.originalEvent.key === 'Enter') simsl(-9999, sli, true);
-    }); // click on dot
-
-    $('li', dots).each(function (index, elm) {
-      return $(elm).click(function () {
-        return simsl(index, sli, true);
-      });
-    }); // press keyboard left-right arrow keys
-
-    $('body').on('keydown', function (e) {
+    });
+    // click on dot
+    $('li', dots).each((index, elm) => $(elm).click(() => simsl(index, sli, true)));
+    // press keyboard left-right arrow keys
+    $('body').on('keydown', e => {
       if (e.originalEvent.key === 'ArrowLeft' || e.originalEvent.key === 'Left') simsl(-9999, sli, true);
       if (e.originalEvent.key === 'ArrowRight' || e.originalEvent.key === 'Right') simsl(9999, sli, true);
-    }); // swipe for touch devices
-
-    var xDown = null;
-    var xDiff = null;
-    var timeDown = null;
-    var startEl = null;
-
-    var handleTouchStart = function handleTouchStart(e) {
+    });
+    // swipe for touch devices
+    let xDown = null;
+    let xDiff = null;
+    let timeDown = null;
+    let startEl = null;
+    const handleTouchStart = e => {
       startEl = e.target;
       timeDown = Date.now();
       xDown = e.touches[0].clientX;
       xDiff = 0;
-    };
-
-    var handleTouchMove = function handleTouchMove(e) {
+    }
+    const handleTouchMove = e => {
       xDiff = xDown - e.touches[0].clientX;
-    };
-
-    var handleTouchEnd = function handleTouchEnd(e) {
+    }
+    const handleTouchEnd = e => {
       if (startEl !== e.target) return;
-      var swipeThreshold = 25; // px
-
-      var swipeTimeout = 500; // ms
-
-      if (Math.abs(xDiff) > swipeThreshold && Date.now() - timeDown < swipeTimeout) {
+      const swipeThreshold = 25; // px
+      const swipeTimeout = 500; // ms
+      if (Math.abs(xDiff) > swipeThreshold && (Date.now() - timeDown) < swipeTimeout) {
         if (xDiff > 0) {
           simsl(9999, sli, true);
         } else {
           simsl(-9999, sli, true);
         }
       }
-    };
-
+    }
     ss[0].addEventListener('touchstart', handleTouchStart, false);
     ss[0].addEventListener('touchmove', handleTouchMove, false);
     ss[0].addEventListener('touchend', handleTouchEnd, false);
-  }); // keyboard focus
-
-  var simslkey = function simslkey() {
+  });
+  // keyboard focus
+  const simslkey = () => {
     $('.js-simple-slider').addClass('ss-keyboard');
     $('body').off('keydown', simslkey);
-  };
-
+  }
   $('body').on('keydown', simslkey);
+
 })();
