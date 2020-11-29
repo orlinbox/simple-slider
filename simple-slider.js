10ccfd9a459e23292000c0668eb133074b55ace0
Orlin Nedyalkov <orlin.nedyalkov@sitecore.com>
2020-11-29 04:18:27 +0200

    Fully independent.

--- simple-slider.js
+++ simple-slider.js
@@ -4,70 +4,68 @@
 MIT License | Copyright (c) 2021 | orlinbox | https://github.com/orlinbox/simple-slider
 SS version 3.0
 */
-
-/*
-  <div
-    class="js-simple-slider js-ss-nojs"
-    data-ss-delay="7500" role="region"
-    aria-label="Simple slider"
-  >
-    <ul class="js-ss">
-      <li>Slide content</li>
-    </ul>
-    <!-- wrapper is optional, navigation elements are optional -->
-    <div class="ss-nav-wrap">
-      <div class="js-ss-prev" tabindex="0" role="button" aria-label="Previous slide">Prev</div>
-      <ul class="js-ss-nav" aria-hidden="true"></ul>
-      <div class="js-ss-next" tabindex="0" role="button" aria-label="Next slide">Next</div>
-    </div>
-  </div>
-*/
 (function () {
-  // elements with focus
-  var foc = 'a[href], area[href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), button:not([disabled]), iframe'; // action function
+  /*
+    <div class="js-simple-slider js-ss-nojs" data-ss-delay="7500" role="region" aria-label="Simple slider">
+      <ul class="js-ss">
+        <li>Slide content 1</li>
+        <li>Slide content 2</li>
+        <li>Slide content 3</li>
+      </ul>
+      <!-- wrapper is optional, navigation elements are optional -->
+      <div class="ss-nav-wrap">
+        <div class="js-ss-prev" tabindex="0" role="button" aria-label="Previous slide">Prev</div>
+        <ul class="js-ss-nav" aria-hidden="true"></ul>
+        <div class="js-ss-next" tabindex="0" role="button" aria-label="Next slide">Next</div>
+      </div>
+    </div>
+  */
+  if (!window.ss) window.ss = {}; // elements with focus
 
-  var simsl = function simsl(goToP, sli, disable) {
-    var goTo = goToP; // disable autorotate feature
+  window.ss.focusElements = 'a[href], area[href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), button:not([disabled]), iframe'; // action method
 
-    if (disable) clearInterval(sli["int"]); // current position
+  window.ss.action = function (goToPosition, slider, disableAutoRotate) {
+    var goTo = goToPosition; // disable autorotate feature
 
-    var pos = parseInt(sli.getAttribute('data-ss-position') - 1, 10); // move logic and action
+    if (disableAutoRotate) clearInterval(slider.ssTimer); // current position
 
-    if (goTo !== pos) {
-      var len = parseInt(sli.getAttribute('data-ss-length'), 10);
+    var positionCurrent = parseInt(slider.getAttribute('data-ss-position') - 1, 10); // move logic and action
+
+    if (goTo !== positionCurrent) {
+      var totalSlides = parseInt(slider.getAttribute('data-ss-length'), 10);
       var goToNext = null;
       var goToPrev = null;
-      if (goTo === 9999) goTo = pos + 1;
-      if (goTo === -9999) goTo = pos - 1;
-      if (goTo > len - 1) goTo = 0;
-      if (goTo < 0) goTo = len - 1;
-      sli.setAttribute('data-ss-position', goTo + 1);
+      if (goTo === 999) goTo = positionCurrent + 1;
+      if (goTo === -999) goTo = positionCurrent - 1;
+      if (goTo > totalSlides - 1) goTo = 0;
+      if (goTo < 0) goTo = totalSlides - 1;
+      slider.setAttribute('data-ss-position', goTo + 1);
       goToNext = goTo + 1;
-      if (goToNext > len - 1) goToNext = 0;
-      if (goToNext < 0) goToNext = len - 1;
+      if (goToNext > totalSlides - 1) goToNext = 0;
+      if (goToNext < 0) goToNext = totalSlides - 1;
       goToPrev = goTo - 1;
-      if (goToPrev > len - 1) goToPrev = 0;
-      if (goToPrev < 0) goToPrev = len - 1;
-      sli.querySelectorAll('.js-ss li').forEach(function (li, index) {
+      if (goToPrev > totalSlides - 1) goToPrev = 0;
+      if (goToPrev < 0) goToPrev = totalSlides - 1;
+      slider.querySelectorAll('.js-ss li').forEach(function (li, index) {
         li.removeAttribute('class');
         li.setAttribute('aria-hidden', true);
-        li.querySelectorAll(foc).forEach(function (item) {
+        li.querySelectorAll(window.ss.focusElements).forEach(function (item) {
           return item.setAttribute('tabindex', -1);
         });
 
         if (index === goTo) {
           li.classList.add('js-ss-sl-current');
           li.removeAttribute('aria-hidden');
-          li.querySelectorAll(foc).forEach(function (item) {
+          li.querySelectorAll(window.ss.focusElements).forEach(function (item) {
             return item.removeAttribute('tabindex');
           });
-          var dots = sli.querySelectorAll('.js-ss-nav li');
+          var dotsForNavigation = slider.querySelectorAll('.js-ss-nav li');
 
-          if (dots.length > 0) {
-            dots.forEach(function (item) {
+          if (dotsForNavigation.length > 0) {
+            dotsForNavigation.forEach(function (item) {
               return item.removeAttribute('class');
             });
-            dots.item(goTo).classList.add('active');
+            dotsForNavigation.item(goTo).classList.add('active');
           }
         } else if (index === goToNext) {
           li.classList.add('js-ss-sl-right');
@@ -78,126 +76,126 @@ SS version 3.0
         }
       });
     }
-  }; // each slider
-
-
-  document.querySelectorAll('.js-simple-slider').forEach(function (sli) {
-    var ss = sli.querySelector('.js-ss');
-    var li = ss.querySelectorAll('li');
-    var count = li.length;
-
-    if (count > 2) {
-      // if we have more than 2 items in the slider
-      sli.classList.remove('js-ss-nojs');
-      sli.setAttribute('data-ss-position', 0);
-      sli.setAttribute('data-ss-length', count);
-      var dots = sli.querySelector('.js-ss-nav') || false;
-      if (dots) li.forEach(function () {
-        return dots.innerHTML += '<li></li>';
-      }); // wrap and clone slider content (for placeholder purposes) with disabled focus
-
-      var wrapEl = document.createElement('div');
-      wrapEl.classList.add('js-ss-wrap');
-      sli.insertBefore(wrapEl, sli.firstChild);
-      wrapEl.appendChild(ss);
-      var ssPl = ss.cloneNode(true);
-      ssPl.querySelectorAll(foc).forEach(function (item) {
-        return item.setAttribute('tabindex', -1);
-      });
-      ssPl.classList.remove('js-ss');
-      ssPl.classList.add('js-ss-placeholder');
-      ssPl.setAttribute('aria-hidden', true);
-      wrapEl.appendChild(ssPl); // init slider
-
-      simsl(0, sli); // autorotate feature
-
-      var timer = sli.getAttribute('data-ss-delay') || 0;
-      if (timer > 0) sli["int"] = setInterval(function () {
-        return simsl(9999, sli);
-      }, timer); // click prev-next image
-
-      li.forEach(function (item) {
-        return item.addEventListener('click', function (elm) {
-          if (elm.currentTarget.classList.contains('js-ss-sl-left')) simsl(-9999, sli, true);
-          if (elm.currentTarget.classList.contains('js-ss-sl-right')) simsl(9999, sli, true);
-        });
-      }); // click prev-next button / press keyboard enter key
-
-      var nextBtn = sli.querySelector('.js-ss-next') || false;
-
-      if (nextBtn) {
-        nextBtn.addEventListener('click', function () {
-          return simsl(9999, sli, true);
-        });
-        nextBtn.addEventListener('keydown', function (e) {
-          if (e.key === 'Enter') simsl(9999, sli, true);
-        });
-      }
-
-      var prevBtn = sli.querySelector('.js-ss-prev') || false;
-
-      if (prevBtn) {
-        prevBtn.addEventListener('click', function () {
-          return simsl(-9999, sli, true);
-        });
-        prevBtn.addEventListener('keydown', function (e) {
-          if (e.key === 'Enter') simsl(-9999, sli, true);
-        });
-      } // click on dot
-
-
-      if (dots) dots.querySelectorAll('li').forEach(function (elm, index) {
-        return elm.addEventListener('click', function () {
-          return simsl(index, sli, true);
-        });
-      }); // press keyboard left-right arrow keys
-
-      document.body.addEventListener('keydown', function (e) {
-        if (e.key === 'ArrowLeft' || e.key === 'Left') simsl(-9999, sli, true);
-        if (e.key === 'ArrowRight' || e.key === 'Right') simsl(9999, sli, true);
-      }); // swipe for touch devices
-
-      var xDown = null;
-      var xDiff = null;
-      var timeDown = null;
-      var startEl = null;
-
-      var handleTouchStart = function handleTouchStart(e) {
-        startEl = e.target;
-        timeDown = Date.now();
-        xDown = e.touches[0].clientX;
-        xDiff = 0;
-      };
-
-      var handleTouchMove = function handleTouchMove(e) {
-        xDiff = xDown - e.touches[0].clientX;
-      };
-
-      var handleTouchEnd = function handleTouchEnd(e) {
-        if (startEl !== e.target) return;
-        var swipeThreshold = 20; // px
-
-        var swipeTimeout = 500; // ms
-
-        if (Math.abs(xDiff) > swipeThreshold && Date.now() - timeDown < swipeTimeout) simsl(xDiff > 0 ? 9999 : -9999, sli, true);
-      };
-
-      ss.addEventListener('touchstart', handleTouchStart, false);
-      ss.addEventListener('touchmove', handleTouchMove, false);
-      ss.addEventListener('touchend', handleTouchEnd, false);
-    } else if (count > 0) {
-      // if we have only 1 or 2 items in the slider
-      sli.classList.remove('js-ss-nojs');
-      sli.classList.add('js-ss-inactive');
-    }
-  }); // keyboard focus
-
-  var simslkey = function simslkey() {
-    document.querySelectorAll('.js-simple-slider').forEach(function (item) {
-      return item.classList.add('ss-keyboard');
-    });
-    document.body.removeEventListener('keydown', simslkey);
   };
 
-  document.body.addEventListener('keydown', simslkey);
+  window.ss.init = function () {
+    // each slider
+    document.querySelectorAll('.js-simple-slider:not(.js-processed)').forEach(function (slider) {
+      var ss = slider.querySelector('.js-ss');
+      var li = ss.querySelectorAll('li');
+      var totalSlides = li.length; // once
+
+      slider.classList.add('js-processed');
+
+      if (totalSlides > 2) {
+        // we have more than 2 items in the slider
+        slider.classList.remove('js-ss-nojs');
+        slider.setAttribute('data-ss-position', 0);
+        slider.setAttribute('data-ss-length', totalSlides);
+        var dotsForNavigation = slider.querySelector('.js-ss-nav') || false;
+        if (dotsForNavigation) li.forEach(function () {
+          return dotsForNavigation.innerHTML += '<li></li>';
+        }); // wrap and clone slider content (for placeholder purposes) with disabled focus
+
+        var wrapperEl = document.createElement('div');
+        wrapperEl.classList.add('js-ss-wrap');
+        slider.insertBefore(wrapperEl, slider.firstChild);
+        wrapperEl.appendChild(ss);
+        var ssPlaceholder = ss.cloneNode(true);
+        ssPlaceholder.querySelectorAll(window.ss.focusElements).forEach(function (item) {
+          return item.setAttribute('tabindex', -1);
+        });
+        ssPlaceholder.classList.remove('js-ss');
+        ssPlaceholder.classList.add('js-ss-placeholder');
+        ssPlaceholder.setAttribute('aria-hidden', true);
+        wrapperEl.appendChild(ssPlaceholder); // init slider
+
+        window.ss.action(0, slider); // autorotate feature
+
+        var timer = slider.getAttribute('data-ss-delay') || 0;
+        if (timer > 0) slider.ssTimer = setInterval(function () {
+          return window.ss.action(999, slider);
+        }, timer); // click prev-next image
+
+        li.forEach(function (item) {
+          return item.addEventListener('click', function (elm) {
+            if (elm.currentTarget.classList.contains('js-ss-sl-left')) window.ss.action(-999, slider, true);
+            if (elm.currentTarget.classList.contains('js-ss-sl-right')) window.ss.action(999, slider, true);
+          });
+        }); // click prev-next button / press keyboard enter key
+
+        var nextBtn = slider.querySelector('.js-ss-next') || false;
+
+        if (nextBtn) {
+          nextBtn.addEventListener('click', function () {
+            return window.ss.action(999, slider, true);
+          });
+          nextBtn.addEventListener('keydown', function (e) {
+            if (e.key === 'Enter') window.ss.action(999, slider, true);
+          });
+        }
+
+        var prevBtn = slider.querySelector('.js-ss-prev') || false;
+
+        if (prevBtn) {
+          prevBtn.addEventListener('click', function () {
+            return window.ss.action(-999, slider, true);
+          });
+          prevBtn.addEventListener('keydown', function (e) {
+            if (e.key === 'Enter') window.ss.action(-999, slider, true);
+          });
+        } // click on navigation dot
+
+
+        if (dotsForNavigation) dotsForNavigation.querySelectorAll('li').forEach(function (elm, index) {
+          return elm.addEventListener('click', function () {
+            return window.ss.action(index, slider, true);
+          });
+        }); // press keyboard left/right arrow keys
+
+        document.body.addEventListener('keydown', function (e) {
+          if (e.key === 'ArrowLeft' || e.key === 'Left') window.ss.action(-999, slider, true);
+          if (e.key === 'ArrowRight' || e.key === 'Right') window.ss.action(999, slider, true);
+        }); // swipe for touch devices
+
+        var xDown = null;
+        var xDiff = null;
+        var timeDown = null;
+        var startEl = null;
+
+        var handleTouchStart = function handleTouchStart(e) {
+          startEl = e.target;
+          timeDown = Date.now();
+          xDown = e.touches[0].clientX;
+          xDiff = 0;
+        };
+
+        var handleTouchMove = function handleTouchMove(e) {
+          xDiff = xDown - e.touches[0].clientX;
+        };
+
+        var handleTouchEnd = function handleTouchEnd(e) {
+          if (startEl !== e.target) return;
+          var swipeThreshold = 20; // px
+
+          var swipeTimeout = 500; // ms
+
+          if (Math.abs(xDiff) > swipeThreshold && Date.now() - timeDown < swipeTimeout) window.ss.action(xDiff > 0 ? 999 : -999, slider, true);
+        };
+
+        ss.addEventListener('touchstart', handleTouchStart, false);
+        ss.addEventListener('touchmove', handleTouchMove, false);
+        ss.addEventListener('touchend', handleTouchEnd, false);
+      } else if (totalSlides > 0) {
+        // we have only 1 or 2 items in the slider
+        slider.classList.remove('js-ss-nojs');
+        slider.classList.add('js-ss-inactive');
+      }
+    });
+  }; // init when DOM is ready
+
+
+  document.addEventListener('DOMContentLoaded', function () {
+    return window.ss.init();
+  });
 })();
--- src/simple-slider.js
+++ src/simple-slider.js
@@ -2,61 +2,60 @@
 MIT License | Copyright (c) 2021 | orlinbox | https://github.com/orlinbox/simple-slider
 SS version 3.0
 */
-/*
-  <div
-    class="js-simple-slider js-ss-nojs"
-    data-ss-delay="7500" role="region"
-    aria-label="Simple slider"
-  >
-    <ul class="js-ss">
-      <li>Slide content</li>
-    </ul>
-    <!-- wrapper is optional, navigation elements are optional -->
-    <div class="ss-nav-wrap">
-      <div class="js-ss-prev" tabindex="0" role="button" aria-label="Previous slide">Prev</div>
-      <ul class="js-ss-nav" aria-hidden="true"></ul>
-      <div class="js-ss-next" tabindex="0" role="button" aria-label="Next slide">Next</div>
-    </div>
-  </div>
-*/
 (() => {
+  /*
+    <div class="js-simple-slider js-ss-nojs" data-ss-delay="7500" role="region" aria-label="Simple slider">
+      <ul class="js-ss">
+        <li>Slide content 1</li>
+        <li>Slide content 2</li>
+        <li>Slide content 3</li>
+      </ul>
+      <!-- wrapper is optional, navigation elements are optional -->
+      <div class="ss-nav-wrap">
+        <div class="js-ss-prev" tabindex="0" role="button" aria-label="Previous slide">Prev</div>
+        <ul class="js-ss-nav" aria-hidden="true"></ul>
+        <div class="js-ss-next" tabindex="0" role="button" aria-label="Next slide">Next</div>
+      </div>
+    </div>
+  */
+  if (!window.ss) window.ss = {};
   // elements with focus
-  const foc = 'a[href], area[href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), button:not([disabled]), iframe';
-  // action function
-  const simsl = (goToP, sli, disable) => {
-    let goTo = goToP;
+  window.ss.focusElements = 'a[href], area[href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), button:not([disabled]), iframe';
+  // action method
+  window.ss.action = (goToPosition, slider, disableAutoRotate) => {
+    let goTo = goToPosition;
     // disable autorotate feature
-    if (disable) clearInterval(sli.int);
+    if (disableAutoRotate) clearInterval(slider.ssTimer);
     // current position
-    const pos = parseInt(sli.getAttribute('data-ss-position') - 1, 10);
+    const positionCurrent = parseInt(slider.getAttribute('data-ss-position') - 1, 10);
     // move logic and action
-    if (goTo !== pos) {
-      const len = parseInt(sli.getAttribute('data-ss-length'), 10);
+    if (goTo !== positionCurrent) {
+      const totalSlides = parseInt(slider.getAttribute('data-ss-length'), 10);
       let goToNext = null;
       let goToPrev = null;
-      if (goTo === 9999) goTo = pos + 1;
-      if (goTo === -9999) goTo = pos - 1;
-      if (goTo > (len-1)) goTo = 0;
-      if (goTo < 0) goTo = len - 1;
-      sli.setAttribute('data-ss-position', goTo + 1);
+      if (goTo === 999) goTo = positionCurrent + 1;
+      if (goTo === -999) goTo = positionCurrent - 1;
+      if (goTo > (totalSlides - 1)) goTo = 0;
+      if (goTo < 0) goTo = totalSlides - 1;
+      slider.setAttribute('data-ss-position', goTo + 1);
       goToNext = goTo + 1;
-      if (goToNext > (len-1)) goToNext = 0;
-      if (goToNext < 0) goToNext = len - 1;
+      if (goToNext > (totalSlides - 1)) goToNext = 0;
+      if (goToNext < 0) goToNext = totalSlides - 1;
       goToPrev = goTo - 1;
-      if (goToPrev > (len-1)) goToPrev = 0;
-      if (goToPrev < 0) goToPrev = len - 1;
-      sli.querySelectorAll('.js-ss li').forEach((li, index) => {
+      if (goToPrev > (totalSlides - 1)) goToPrev = 0;
+      if (goToPrev < 0) goToPrev = totalSlides - 1;
+      slider.querySelectorAll('.js-ss li').forEach((li, index) => {
         li.removeAttribute('class');
         li.setAttribute('aria-hidden', true);
-        li.querySelectorAll(foc).forEach((item) => item.setAttribute('tabindex', -1));
+        li.querySelectorAll(window.ss.focusElements).forEach((item) => item.setAttribute('tabindex', -1));
         if (index === goTo) {
           li.classList.add('js-ss-sl-current');
           li.removeAttribute('aria-hidden');
-          li.querySelectorAll(foc).forEach((item) => item.removeAttribute('tabindex'));
-          const dots = sli.querySelectorAll('.js-ss-nav li');
-          if (dots.length > 0) {
-            dots.forEach((item) => item.removeAttribute('class'));
-            dots.item(goTo).classList.add('active');
+          li.querySelectorAll(window.ss.focusElements).forEach((item) => item.removeAttribute('tabindex'));
+          const dotsForNavigation = slider.querySelectorAll('.js-ss-nav li');
+          if (dotsForNavigation.length > 0) {
+            dotsForNavigation.forEach((item) => item.removeAttribute('class'));
+            dotsForNavigation.item(goTo).classList.add('active');
           }
         } else if (index === goToNext) {
           li.classList.add('js-ss-sl-right');
@@ -68,90 +67,90 @@ SS version 3.0
       });
     }
   }
-  // each slider
-  document.querySelectorAll('.js-simple-slider').forEach((sli) => {
-    const ss = sli.querySelector('.js-ss');
-    const li = ss.querySelectorAll('li');
-    const count = li.length;
-    if (count > 2) {
-      // if we have more than 2 items in the slider
-      sli.classList.remove('js-ss-nojs');
-      sli.setAttribute('data-ss-position', 0);
-      sli.setAttribute('data-ss-length', count);
-      const dots = sli.querySelector('.js-ss-nav') || false;
-      if (dots) li.forEach(() => dots.innerHTML += '<li></li>');
-      // wrap and clone slider content (for placeholder purposes) with disabled focus
-      const wrapEl = document.createElement('div');
-      wrapEl.classList.add('js-ss-wrap');
-      sli.insertBefore(wrapEl, sli.firstChild);
-      wrapEl.appendChild(ss);
-      const ssPl = ss.cloneNode(true);
-      ssPl.querySelectorAll(foc).forEach((item) => item.setAttribute('tabindex', -1));
-      ssPl.classList.remove('js-ss');
-      ssPl.classList.add('js-ss-placeholder');
-      ssPl.setAttribute('aria-hidden', true);
-      wrapEl.appendChild(ssPl);
-      // init slider
-      simsl(0, sli);
-      // autorotate feature
-      const timer = sli.getAttribute('data-ss-delay') || 0;
-      if (timer > 0) sli.int = setInterval(() => simsl(9999, sli), timer);
-      // click prev-next image
-      li.forEach((item) => item.addEventListener('click', (elm) => {
-        if (elm.currentTarget.classList.contains('js-ss-sl-left')) simsl(-9999, sli, true);
-        if (elm.currentTarget.classList.contains('js-ss-sl-right')) simsl(9999, sli, true);
-      }));
-      // click prev-next button / press keyboard enter key
-      const nextBtn = sli.querySelector('.js-ss-next') || false;
-      if (nextBtn) {
-        nextBtn.addEventListener('click', () => simsl(9999, sli, true));
-        nextBtn.addEventListener('keydown', (e) => { if (e.key === 'Enter') simsl(9999, sli, true) });
+  window.ss.init = () => {
+    // each slider
+    document.querySelectorAll('.js-simple-slider:not(.js-processed)').forEach((slider) => {
+      const ss = slider.querySelector('.js-ss');
+      const li = ss.querySelectorAll('li');
+      const totalSlides = li.length;
+      // once
+      slider.classList.add('js-processed');
+      if (totalSlides > 2) {
+        // we have more than 2 items in the slider
+        slider.classList.remove('js-ss-nojs');
+        slider.setAttribute('data-ss-position', 0);
+        slider.setAttribute('data-ss-length', totalSlides);
+        const dotsForNavigation = slider.querySelector('.js-ss-nav') || false;
+        if (dotsForNavigation) li.forEach(() => dotsForNavigation.innerHTML += '<li></li>');
+        // wrap and clone slider content (for placeholder purposes) with disabled focus
+        const wrapperEl = document.createElement('div');
+        wrapperEl.classList.add('js-ss-wrap');
+        slider.insertBefore(wrapperEl, slider.firstChild);
+        wrapperEl.appendChild(ss);
+        const ssPlaceholder = ss.cloneNode(true);
+        ssPlaceholder.querySelectorAll(window.ss.focusElements).forEach((item) => item.setAttribute('tabindex', -1));
+        ssPlaceholder.classList.remove('js-ss');
+        ssPlaceholder.classList.add('js-ss-placeholder');
+        ssPlaceholder.setAttribute('aria-hidden', true);
+        wrapperEl.appendChild(ssPlaceholder);
+        // init slider
+        window.ss.action(0, slider);
+        // autorotate feature
+        const timer = slider.getAttribute('data-ss-delay') || 0;
+        if (timer > 0) slider.ssTimer = setInterval(() => window.ss.action(999, slider), timer);
+        // click prev-next image
+        li.forEach((item) => item.addEventListener('click', (elm) => {
+          if (elm.currentTarget.classList.contains('js-ss-sl-left')) window.ss.action(-999, slider, true);
+          if (elm.currentTarget.classList.contains('js-ss-sl-right')) window.ss.action(999, slider, true);
+        }));
+        // click prev-next button / press keyboard enter key
+        const nextBtn = slider.querySelector('.js-ss-next') || false;
+        if (nextBtn) {
+          nextBtn.addEventListener('click', () => window.ss.action(999, slider, true));
+          nextBtn.addEventListener('keydown', (e) => { if (e.key === 'Enter') window.ss.action(999, slider, true) });
+        }
+        const prevBtn = slider.querySelector('.js-ss-prev') || false;
+        if (prevBtn) {
+          prevBtn.addEventListener('click', () => window.ss.action(-999, slider, true));
+          prevBtn.addEventListener('keydown', (e) => { if (e.key === 'Enter') window.ss.action(-999, slider, true) });
+        }
+        // click on navigation dot
+        if (dotsForNavigation) dotsForNavigation.querySelectorAll('li').forEach((elm, index) => elm.addEventListener('click', () => window.ss.action(index, slider, true)));
+        // press keyboard left/right arrow keys
+        document.body.addEventListener('keydown', (e) => {
+          if (e.key === 'ArrowLeft' || e.key === 'Left') window.ss.action(-999, slider, true);
+          if (e.key === 'ArrowRight' || e.key === 'Right') window.ss.action(999, slider, true);
+        })
+        // swipe for touch devices
+        let xDown = null;
+        let xDiff = null;
+        let timeDown = null;
+        let startEl = null;
+        const handleTouchStart = (e) => {
+          startEl = e.target;
+          timeDown = Date.now();
+          xDown = e.touches[0].clientX;
+          xDiff = 0;
+        }
+        const handleTouchMove = (e) => {
+          xDiff = xDown - e.touches[0].clientX;
+        }
+        const handleTouchEnd = (e) => {
+          if (startEl !== e.target) return;
+          const swipeThreshold = 20; // px
+          const swipeTimeout = 500; // ms
+          if (Math.abs(xDiff) > swipeThreshold && (Date.now() - timeDown) < swipeTimeout) window.ss.action((xDiff > 0) ? 999 : -999, slider, true);
+        }
+        ss.addEventListener('touchstart', handleTouchStart, false);
+        ss.addEventListener('touchmove', handleTouchMove, false);
+        ss.addEventListener('touchend', handleTouchEnd, false);
+      } else if (totalSlides > 0) {
+        // we have only 1 or 2 items in the slider
+        slider.classList.remove('js-ss-nojs');
+        slider.classList.add('js-ss-inactive');
       }
-      const prevBtn = sli.querySelector('.js-ss-prev') || false;
-      if (prevBtn) {
-        prevBtn.addEventListener('click', () => simsl(-9999, sli, true));
-        prevBtn.addEventListener('keydown', (e) => { if (e.key === 'Enter') simsl(-9999, sli, true) });
-      }
-      // click on dot
-      if (dots) dots.querySelectorAll('li').forEach((elm, index) => elm.addEventListener('click', () => simsl(index, sli, true)));
-      // press keyboard left-right arrow keys
-      document.body.addEventListener('keydown', (e) => {
-        if (e.key === 'ArrowLeft' || e.key === 'Left') simsl(-9999, sli, true);
-        if (e.key === 'ArrowRight' || e.key === 'Right') simsl(9999, sli, true);
-      })
-      // swipe for touch devices
-      let xDown = null;
-      let xDiff = null;
-      let timeDown = null;
-      let startEl = null;
-      const handleTouchStart = (e) => {
-        startEl = e.target;
-        timeDown = Date.now();
-        xDown = e.touches[0].clientX;
-        xDiff = 0;
-      }
-      const handleTouchMove = (e) => {
-        xDiff = xDown - e.touches[0].clientX;
-      }
-      const handleTouchEnd = (e) => {
-        if (startEl !== e.target) return;
-        const swipeThreshold = 20; // px
-        const swipeTimeout = 500; // ms
-        if (Math.abs(xDiff) > swipeThreshold && (Date.now() - timeDown) < swipeTimeout) simsl((xDiff > 0) ? 9999 : -9999, sli, true);
-      }
-      ss.addEventListener('touchstart', handleTouchStart, false);
-      ss.addEventListener('touchmove', handleTouchMove, false);
-      ss.addEventListener('touchend', handleTouchEnd, false);
-    } else if (count > 0) {
-      // if we have only 1 or 2 items in the slider
-      sli.classList.remove('js-ss-nojs');
-      sli.classList.add('js-ss-inactive');
-    }
-  });
-  // keyboard focus
-  const simslkey = () => {
-    document.querySelectorAll('.js-simple-slider').forEach((item) => item.classList.add('ss-keyboard'));
-    document.body.removeEventListener('keydown', simslkey);
+    });
   }
-  document.body.addEventListener('keydown', simslkey);
+  // init when DOM is ready
+  document.addEventListener('DOMContentLoaded', () => window.ss.init());
 })();
